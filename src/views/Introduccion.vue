611292f0e471c340f03f1b5d0eeca47af1c262fd
Luis Perez <ljpm9401@gmail.com>
2024-11-20 18:27:00 -0500

    Vinculación video

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -31,7 +31,7 @@
     
     figure.mt-4(data-aos="zoom-in-up" data-aos-duration="800")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/mxb4fQq2CNw" title="Integración y gestión avanzada de datos para inteligencia artificial" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
 
     
